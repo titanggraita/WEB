8981da8c1f7cba152135db3d29cac8b27b3092e1
titanggraita <46990902+titanggraita@users.noreply.github.com>
2019-07-08 12:27:02 +0700

    kayanya gini aja dulu tampilannya

--- application/views/search_faq.php
+++ application/views/search_faq.php
@@ -1,3 +1,23 @@
-<body>
-
-</body>
+<nav class="navbar navbar-expand-lg bg-nav fixed-top">
+  <a class="navbar-brand" style="margin-left:50px;margin-top:-30px;" href="<?= base_url() ?>C_Admin"><img src="<?= base_url() ?>assets/gambar/batan1.png" class="img-brand"></a>
+  <div class="row">
+      <h4 style="color:#EEE8AA; margin-left:30px;margin-top:10px;">HASIL SEARCH</h4>
+    <div class="topnav" style="margin-left:550px;font-size:15px;">
+      <a href="<?=base_url()?>user/index" class="active">Home</a>
+      <a href="<?=base_url()?>user/about">About</a>
+      <a href="<?=base_url()?>user/policies">Policies</a>
+      <a href="<?=base_url()?>user/contact">Contact Us</a>
+    </div>
+  </div>
+  <div style="margin-bottom:100px;"></div>
+  <hr>
+</nav>
+<div class="container-fluid" style="margin-top:100px; padding:10px 10px">
+      <div class="input-group">
+      	<input type="text" class="form-control" style="border-radius:40px;" placeholder="Search for articles ...">
+        <span class="input-group-btn">
+          <button class="btn btn-search" type="button"><i class="fa fa-search fa-fw"></i> Search</button>
+        </span>
+      </div>
+    </div>
+</div>
