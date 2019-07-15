e9536ef0e5bb5c5bb3098f572b276b2fdb0458ee
titanggraita <46990902+titanggraita@users.noreply.github.com>
2019-07-15 11:14:36 +0700

    Updatee

--- application/models/Faq_model.php
+++ application/models/Faq_model.php
@@ -1,14 +1,93 @@
 
-<?php
-class Faq_model extends CI_Model
-{
+<?php 
+	class Faq_model extends CI_Model{
+		
+		public function get_faq_keyword($keyword){
+			$this->db->like('pertanyaan', $keyword);
+			$this->db->or_like('jawaban', $keyword);
+			$this->db->or_like('keyword', $keyword);
+			return $this->db->get('faq')->result_array();;
+		}
+		public function getAllQuestionFromFAQ(){
+        	$que = $this->db->get('faq');
+        	return $que->result_array();
+		}
+		public function getAllQuestionFromOP()
+    	{
+        $que = $this->db->get('open_question');
+        return $que->result_array();
+		}
+		public function getFaqById($id)
+		{
+		
+			return $this->db->get_where('faq', ['ID_faq' => $id])->row();
+		}
+		public function getOPById($id)
+		{
+		
+			return $this->db->get_where('open_question', ['ID_question' => $id])->row();
+    	}
+		public function AnswerOpenQuestion($ID_question)
+		{
+			$data = [
+				"pertanyaan" => $this->input->post('pertanyaan', true),
+				"jawaban" => $this->input->post('jawaban', true)
+			];
+		
+			$this->db->where('ID_question',$ID_question);
+			$this->db->update('open_question',$data);
+		}
+		public function hapusOpenQuestion($ID_question)
+		{
+			return $this->db->get($ID_question);
+		}
+		public function getQuestionById($id)
+		{
+			return $this->db->get_where('open_question', ['ID_question' => $id])->row();
+		}
+		public function tambahQuestion(){
+			$data = [
+				"username" => $this->input->post('username', true),
+				"pertanyaan" => $this->input->post('pertanyaan', true)
+			];
+			return $this->db->insert('open_question', $data);
+		}
+		public function ubahDataFaq($ID_faq){
+			$data = [
+				"pertanyaan" => $this->input->post('pertanyaan', true),
+            	"jawaban" => $this->input->post('jawaban', true),
+				"keyword" => $this->input->post('keyword', true),
+				"author" => $this->input->post('author', true),
+				"status" => $this->input->post('status', true)
+			];
+			$this->db->where('ID_faq',$ID_faq);
+			$this->db->update('faq',$data);
+		}
+		public function tambahFaq(){
+			$data = [
+				"pertanyaan" => $this->input->post('pertanyaan', true),
+            	"jawaban" => $this->input->post('jawaban', true),
+				"keyword" => $this->input->post('keyword', true),
+				"author" => $this->input->post('author', true),
+				"status" => $this->input->post('status', true)
+			];
+			$this->db->insert('faq', $data);
+		}
+		public function publish($ID_question)
+		{
+			$data = [
+				"pertanyaan" => $this->input->post('pertanyaan', true),
+            	"jawaban" => $this->input->post('jawaban', true),
+				"keyword" => $this->input->post('keyword', true),
+				"author" => $this->input->post('author', true),
+				"status" => $this->input->post('status', true)
+			];
+		
+			$this->db->where('ID_question',$ID_question);
+			$this->db->insert('faq',$data);
+		}
 
-	public function get_faq_keyword($keyword)
-	{
-		$this->db->like('pertanyaan', $keyword);
-		$this->db->or_like('jawaban', $keyword);
-		$this->db->or_like('keyword', $keyword);
-		return $this->db->get('faq')->result_array();
-	}
-}
+		
+		
+    }
 ?>
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -10,8 +10,9 @@ class User_model extends CI_Model
         $this->db->where('Username', $data["Username"]);
         $this->db->where('Password', $data["Password"]);
         $que = $this->db->get("user");
-        return $que->num_rows();
+        return $que->row_array();
     }
+    
     public function getAllUser()
     {
         $que = $this->db->get('User');
@@ -22,6 +23,7 @@ class User_model extends CI_Model
         $que = $this->db->get('kompetensi_kontributor');
         return $que->result_array();
     }
+    
     public function tambahUser(){
         $data = [
             "Username" => $this->input->post('Username', true),
@@ -31,7 +33,24 @@ class User_model extends CI_Model
             "Institution" => $this->input->post('Institution', true),
             "Job" => $this->input->post('Job', true),
             "Province" => $this->input->post('Province', true),
-            "State" => $this->input->post('State', true)
+            "State" => $this->input->post('State', true),
+            "ID_level"=> 3,
+            "Aktivasi"=> 'T'
+        ];
+        $this->db->insert('user', $data);
+    }
+    public function tambahContributor(){
+        $data = [
+            "Username" => $this->input->post('Username', true),
+            "Password" => $this->input->post('Password', true),
+            "Email" => $this->input->post('Email', true),
+            "Nama" => $this->input->post('Nama', true),
+            "Institution" => $this->input->post('Institution', true),
+            "Job" => $this->input->post('Job', true),
+            "Province" => $this->input->post('Province', true),
+            "State" => $this->input->post('State', true),
+            "ID_level"=> 2,
+            "Aktivasi"=> 'Y'
         ];
         $this->db->insert('user', $data);
     }
@@ -64,12 +83,8 @@ class User_model extends CI_Model
 		$this->db->update('user',$data);
     }
     
-    public function tambahQuestion(){
-        $data = [
-            "username" => $this->input->post('username', true),
-            "pertanyaan" => $this->input->post('pertanyaan', true)
-        ];
-        return $this->db->insert('open_question', $data);
-    }
+    
 
-}
+}  
+ 
+
